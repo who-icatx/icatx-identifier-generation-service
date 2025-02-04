8f9f0b10ae63a4354a1703502cf883f6aba4bcf5
soimugeo <soimugeo@gmail.com>
2025-02-04 15:18:10 +0200

    [https://github.com/who-icatx/icatx-project/issues/66]: fixed IdHelper

--- src/main/java/edu/stanford/protege/webprotege/identity/ids/IdHelper.java
+++ src/main/java/edu/stanford/protege/webprotege/identity/ids/IdHelper.java
@@ -4,23 +4,30 @@ import java.nio.charset.StandardCharsets;
 import java.security.*;
 
 public class IdHelper {
-    public static String hashSeed(long seedValue) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hashBytes = digest.digest(String.valueOf(seedValue).getBytes(StandardCharsets.UTF_8));
 
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : hashBytes) {
-                hexString.append(String.format("%02x", b));
-            }
-            return hexString.toString();
+    static MessageDigest messageDigest;
+
+    static {
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Error generating hash", e);
         }
     }
 
+    public static String hashSeed(long seedValue) {
+        byte[] hashBytes = messageDigest.digest(String.valueOf(seedValue).getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hashBytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+
+    }
+
     public static String extractNineDigitNumberInStringFromHash(String hash) {
-        return String.format("%09d",Long.parseLong(hash.substring(0, 9), 16) % 1_000_000_000L);
+        return String.format("%09d", Long.parseLong(hash.substring(0, 9), 16) % 1_000_000_000L);
     }
 
 }
