42b48f32183b2c1ad23111a07260295389f1a059
alexsilaghi <silaghialexmihail@gmail.com>
2025-05-08 10:50:18 +0300

    add correlation id. bump ipc.

--- src/main/java/edu/stanford/protege/webprotege/identity/CommandsController.java
+++ src/main/java/edu/stanford/protege/webprotege/identity/CommandsController.java
@@ -2,10 +2,12 @@ package edu.stanford.protege.webprotege.identity;
 
 import edu.stanford.protege.webprotege.identity.ids.IdGenerationService;
 import edu.stanford.protege.webprotege.identity.project.ProjectService;
+import edu.stanford.protege.webprotege.ipc.util.CorrelationMDCUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/identity-commands")
@@ -25,6 +27,9 @@ public class CommandsController {
 
     @PostMapping(value = "/register-projects")
     public ResponseEntity<List<String>> registerProjects(@RequestBody List<String> projectIds) {
+        CorrelationMDCUtil.setCorrelationId(UUID.randomUUID().toString());
+
+
         List<String> registeredProjects = projectService.registerProjects(projectIds);
         return ResponseEntity.ok(registeredProjects);
     }
@@ -32,6 +37,8 @@ public class CommandsController {
 
     @GetMapping("/id")
     public String generateId(@RequestBody String prefix) {
+        CorrelationMDCUtil.setCorrelationId(UUID.randomUUID().toString());
+
         return idGenerationService.generateUniqueId(prefix);
     }
 }
